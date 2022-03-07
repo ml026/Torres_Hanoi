b7082f5168eb84134187dbae76bc24e6fd275574
María Lahuerta Magraner <mlahmag@upv.edu.es>
2022-03-07 16:57:14 +0100

    Implementación program

--- Torres_de_Hanoi/Program.cs
+++ Torres_de_Hanoi/Program.cs
@@ -11,9 +11,20 @@ namespace Torres_de_Hanoi
         static void Main(string[] args)
         {
 
+            Console.WriteLine("¿Cuántos discos quieres?:");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Pila ini = new Pila(num, "ini");
+            Pila aux = new Pila("aux");
+            Pila fin = new Pila("fin");
+
+            int m = Hanoi.iterativo(num, ini, fin, aux);
+            Console.WriteLine("Utiliza" + " " + m + " " + "movimientos");
+
+
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
+            Console.ReadKey(); //Espera U presione tecla
         }
     }
 }
