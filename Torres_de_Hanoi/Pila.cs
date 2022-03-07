b00adaa8dc1a2022854bc14140c9f5369ff46e09
María Lahuerta Magraner <mlahmag@upv.edu.es>
2022-03-07 16:50:01 +0100

    Clase Pila

--- Torres_de_Hanoi/Pila.cs
+++ Torres_de_Hanoi/Pila.cs
@@ -8,35 +8,87 @@ namespace Torres_de_Hanoi
 {
     class Pila
     {
-        public int Size { get; set; }
-        /* TODO: Elegir tipo de Top
-        public int Top { get; set; }
-        public String Top { get; set; }        
-        */
-        /* TODO: Elegir tipo de Elementos
-        public Disco[] Elementos { get; set; }
-        public List<Disco> Elementos { get; set; }
-        */
 
-        /* TODO: Implementar métodos */
-        public Pila()
+        //Creamos un list de discos. LLamado elementos
+        public List<Disco> Elementos { get; set; }
+
+        //Atributo que proporciona nombre a la pila
+        //No get ; set porque solo consultar top (aunque puede tenerlo)
+        public string Nombre { get; set; }
+
+
+
+        //Creamos el constructor, que creará las dos pilas vacías
+        public Pila(string n)
         {
+            Nombre = n;
+            Elementos = new List<Disco>();
 
         }
 
+        //Creamos el constructor de la pila, que creará la pila inicial
+        public Pila(int w, string n)
+        {
+            Nombre = n;
+            Elementos = new List<Disco>();
+            for (int i = w; i > 0; i--)
+            {
+                //Crea los diferentes  discos, con el tamaño que nos indica el for
+                Disco d = new Disco();
+                d.valor = i;
+                push(d);
+            }
+        }
+
+        //Método que nos devuelve el tamaño de la pila
+        public int Size { get { return Elementos.Count; } }
+
+        //Método que nos indica el elemento que hay a la parte de arriba
+        public Disco Top
+        {
+            get
+            {    //Comprueba si la pila está llena
+                if (isEmpty() == false)
+                {
+                    //Si lo está devuelve el elemento que tenga más arriba 
+                    return Elementos[Size - 1];
+                }
+                //Si no devuelve null, ya que estará vacía
+                return null;
+
+            }
+        }
+
+
+        //Método que añade los elementos a la pila
         public void push(Disco d)
         {
+            Elementos.Add(d);
 
         }
 
+
+        //Método que cogeel elemento a mover
         public Disco pop()
         {
-            return null;
-        }                
+            //Se guarda el disco a mover, para no perderlo
+            Disco dTop = Top;
+            //Pude ser 0
+            //Elimina el disco de la columna en la que estaba
+            Elementos.RemoveAt(Size - 1);
+            return dTop;
+        }
 
+        //Comprueba si la pila está vacía
         public bool isEmpty()
         {
-            return true;
+            if (Size == 0)
+            {
+                return true;
+            }
+            return false;
+
+
         }
 
     }
