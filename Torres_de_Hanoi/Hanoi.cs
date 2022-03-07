1740c3b06adfb38374e21c0adc81297428902732
María Lahuerta Magraner <mlahmag@upv.edu.es>
2022-03-07 16:53:37 +0100

    Clase Hanoi con iterativo

--- Torres_de_Hanoi/Hanoi.cs
+++ Torres_de_Hanoi/Hanoi.cs
@@ -6,18 +6,91 @@ using System.Threading.Tasks;
 
 namespace Torres_de_Hanoi
 {
-    class Hanoi
+    static class Hanoi
     {
-        /*TODO: Implementar métodos*/
-        public void mover_disco(Pila a, Pila b)
-        {
 
+        private static int numDiscos;
+        private static Pila ini, fin, aux;
+        //Atributo x 
+        private static int numMovimientos;
+
+        /*TODO: Implementar métodos*/
+        private static void mover_disco(Pila a, Pila b)
+        {
+            if (fin.Size != numDiscos)
+            {
+                //Se mueve el del push . Fin, también el del push
+                //a ->b
+                if (!a.isEmpty())
+                {
+                    if (b.isEmpty())
+                    {
+                        b.push(a.pop());
+                        numMovimientos++;
+                        Console.WriteLine("Se ha movido" + b.Top.valor + " " + "de" + " " + a.Nombre + " " + "a" + " " + b.Nombre);
+                    }
+                    else if (a.Top.valor < b.Top.valor)
+                    {
+                        b.push(a.pop());
+                        numMovimientos++;
+                        Console.WriteLine("Se ha movido" + b.Top.valor + " " + "de" + " " + a.Nombre + " " + "a" + " " + b.Nombre);
+                    }
+                    else
+                    {
+                        a.push(b.pop());
+                        numMovimientos++;
+                        Console.WriteLine("Se ha movido" + a.Top.valor + " " + "de" + " " + b.Nombre + " " + "a" + " " + a.Nombre);
+                    }
+
+                }
+
+
+
+                else //a vacia
+                {
+                    if (!b.isEmpty())
+                    {
+                        a.push(b.pop());
+                        numMovimientos++;
+                        Console.WriteLine("Se ha movido" + a.Top.valor + " " + "de" + " " + b.Nombre + "a" + " " + a.Nombre);
+                    }
+                }
+            }
         }
 
-        public int iterativo(int n, Pila ini, Pila fin, Pila aux)
+        public static int iterativo(int n, Pila INI, Pila FIN, Pila AUX)
         {
-            return 0;
+            numDiscos = n;
+            numMovimientos = 0;
+            ini = INI;
+            fin = FIN;
+            aux = AUX;
+            //No ini pilas, ni atributo ->fin.size!=n
+            if (n % 2 != 0)
+            {
+                while (fin.Size != numDiscos)
+                {
+                    mover_disco(ini, fin);
+                    mover_disco(ini, aux);
+                    mover_disco(aux, fin);
+                }
+
+            }
+
+            else
+            {
+                while (fin.Size != numDiscos)
+                {
+                    mover_disco(ini, aux);
+                    mover_disco(ini, fin);
+                    mover_disco(aux, fin);
+
+                }
+            }
+            return numMovimientos;
         }
 
     }
 }
+
+
